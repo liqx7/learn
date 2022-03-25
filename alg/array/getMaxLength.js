ee32488370f99da830acb47fb4fd5480ed4e7774
Li Qinxin <liqinxin@hyperchain.cn>
2022-03-25 10:59:48 +0800

    feat: array

---
+++ alg/array/getMaxLength.js
@@ -0,0 +1,27 @@
+function getMaxLength(arr, k) {
+  if (arr == null || arr.length === 0 || k <= 0) {
+    return 0;
+  }
+
+  let left = 0,
+    right = 0;
+  let sum = arr[0];
+  let len = 0;
+
+  while (right < arr.length) {
+    if (sum === k) {
+      len = Math.max(len, right - left + 1);
+      sum -= arr[left++];
+    } else if (sum < k) {
+      right++;
+      if (right === arr.length) {
+        break;
+      }
+      sum += arr[right];
+    } else {
+      sum -= arr[left++];
+    }
+  }
+
+  return len;
+}
---
+++ js/RecordAndTuple.js
@@ -0,0 +1,7 @@
+function a(str) {
+  console.log(str);
+}
+
+var a1 = (0, a);
+
+console.log(a1);
